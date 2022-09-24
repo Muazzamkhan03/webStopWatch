1de0d978d08c495f3b9d3d0677ff267a79612c80
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-24 23:06:21 +0500

    Added event listener to start button that runs a function named time at an interval of 10 milliseconds

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 let min = document.querySelector(".min");
 let sec = document.querySelector(".sec");
 let tenth = document.querySelector(".tenth");
+let interval;
+
+document.querySelector("#start").addEventListener('click',()=>{
+    clearInterval(interval);
+    interval = setInterval(time,10);
+});
+
 
